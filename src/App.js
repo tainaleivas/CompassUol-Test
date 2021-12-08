24799c58842b89c6856798130570beef1503291c
Tainá <leivastaina@gmail.com>
2021-12-07 21:22:53 -0300

    feat: add paragraph component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Input from './components/Input/Input'
+import Paragraph from './components/Paragraph/Paragraph'
 
 const App = () => {
   return (
     <div>
       <Input/>
+      <Paragraph text="Teste"/>
     </div>
   )
 }
